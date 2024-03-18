aa634d20e4557c63424ed651c3dd7092d61be682
Balbib99 <balbib99@gmail.com>
2024-03-18 13:54:31 +0100

    logs repartidos por signin.ts

--- src/pages/api/auth/signin.ts
+++ src/pages/api/auth/signin.ts
@@ -7,6 +7,8 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     const password = formData.get("password")?.toString();
 
     if (!email || !password) {
+        console.log("Se requieren password e email");
+        
         return new Response("Email and password are required", { status: 400 });
     }
 
@@ -16,16 +18,22 @@ export const POST: APIRoute = async ({ request, cookies, redirect }) => {
     });
 
     if (error) {
+        console.log("Hemos tenido un error en el signin");
+        
         return new Response(error.message, { status: 500 });
     }
 
     const { access_token, refresh_token } = data.session;
+
+    console.log("access token: " + access_token);
+    console.log("refresh token: " + refresh_token);
+    
     cookies.set("sb-access-token", access_token, {
         path: "/",
     });
     cookies.set("sb-refresh-token", refresh_token, {
         path: "/",
     });
-    
+
     return redirect("/dashboard");
 };
