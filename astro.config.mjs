61dfe0103001562db7ad697cccbb83bd7af43147
Balbib99 <balbib99@gmail.com>
2024-03-19 12:59:36 +0100

    Cambiado de nuevo astro.config.mjs para static

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
 
-// Define un adapter basado en el entorno para determinar si estamos en Vercel o localmente
-const adapter = process.env.VERCEL ? "vercel/serverless" : "astro";
-const integrations = [tailwind(), react()];
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
-  integrations,
-  output: "dist",
-  // Utiliza el adapter correspondiente
-  adapter
+  integrations: [tailwind(), react()],
+  output: "static",
+  adapter: vercel()
 });
